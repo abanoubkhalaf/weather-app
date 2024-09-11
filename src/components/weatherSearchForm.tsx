99a4e8ba4b181836616b96b45bfc3d62e7f17afc
Abanoub Khalaf <54329889+abanoubkhalaf@users.noreply.github.com>
2024-09-11 13:36:43 +0300

    Update weatherSearchForm.tsx

--- src/components/weatherSearchForm.tsx
+++ src/components/weatherSearchForm.tsx
@@ -7,17 +7,17 @@ const WeatherSearchForm: FC<WeatherSearchFormProps> = ({
   handleSubmit,
 }) => {
   return (
-    <form onSubmit={handleSubmit} className="mb-4">
+    <form onSubmit={handleSubmit} className="flex mb-4">
       <input
         type="text"
-        className="w-3/4 p-3 border rounded-l-lg focus:outline-none focus:ring focus:border-blue-500"
+        className="w-full p-3 border rounded-l-lg w-4/4 focus:outline-none focus:ring focus:border-blue-500"
         value={city}
         onChange={(e) => setCity(e.target.value)}
         placeholder="Enter city name"
       />
       <button
         type="submit"
-        className="p-3 text-white transition bg-blue-500 rounded-r-lg hover:bg-blue-600 disabled:bg-blue-300"
+        className="p-3 text-white transition bg-blue-500 rounded-r-lg hover:bg-blue-600 disabled:bg-blue-300 whitespace-nowrap"
         disabled={city.trim().length === 0}
       >
         Get Weather
